9e9e6cc78fcc7ca95b936470a4056553d89aaaca
nati-menashe <nmenashe@medallia.com>
2022-05-10 22:29:01 +0300

    first commit

---
+++ src/com/medallia/DataStructure.java
@@ -0,0 +1,20 @@
+package com.medallia;
+
+public class DataStructure {
+
+    public boolean insert(Integer x) {
+        return false;
+    }
+
+    public boolean remove(Integer x) {
+        return false;
+    }
+
+    public Integer getRandom(){
+        return null;
+    }
+
+    public int search(Integer x) {
+        return 0;
+    }
+}
---
+++ src/com/medallia/Main.java
@@ -0,0 +1,9 @@
+package com.medallia;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        DataStructure dataStructure = new DataStructure();
+    }
+}
